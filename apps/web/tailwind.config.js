bab55d3eb27e6e3c69138ce87e27099d539648c6
Robert <velto@o2.pl>
2023-02-23 17:31:14 +0100

    feat: config tailwind theme

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      black: '#242331',
+      gray: '#797979',
+      'gray-light': '#F9F9F9',
+      purple: '#5236FF',
+      'purple-light': '#EFECFF',
+    },
+    fontSize: {
+      '4xl': '4.6875rem',
+      '3xl': '3.125rem',
+      '2xl': '2.5rem',
+      xl: '1.875rem',
+      lg: '1.5625rem',
+      base: '1.375rem',
+      sm: '1.25rem',
+      xs: '1.125rem',
+    },
+    extend: {},
+  },
+  plugins: [],
 };
